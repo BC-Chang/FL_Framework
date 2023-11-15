5550d43cc03304454533e18a86473418df7b661f
BCChang <bcchang@utexas.edu>
2023-11-15 12:05:50 -0600

    Add data input file argparse to client

--- client.py
+++ client.py
@@ -4,35 +4,28 @@ import flwr as fl
 import torch
 import argparse
 
+def MSNet_Client():
+
+
 
 def main() -> None:
     # Parse command line argument `partition`
     parser = argparse.ArgumentParser(description="Flower Client")
     parser.add_argument(
         "--input_data",
-        type=int,
-        default=0,
-        choices=range(0, 10),
-        required=False,
-        help="Specifies the artificial data partition of CIFAR10 to be used. \
-        Picks partition 0 by default",
-    )
-    parser.add_argument(
-        "--use_cuda",
-        type=bool,
-        default=False,
-        required=False,
-        help="Set to true to use GPU. Default: False",
+        type=str,
+        required=True,
+        help="Specify the location of the data input yaml with listed data files.",
     )
 
     args = parser.parse_args()
 
     # TODO: Load data from a specific datafile
     # Load local data partition
-    trainset, testset = load_data.load_partition(args.partition)
+    trainloader, valloader = load_data.load_partition(args.partition)
 
     # TODO: Instantiate Flower client
-    client = MSNet_Client(trainset, testset, device)
+    client = MSNet_Client(trainloader, valloader, device)
 
     # Start Flower client
     # TODO: Check server address
@@ -40,4 +33,5 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
 
