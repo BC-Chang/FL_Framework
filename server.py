4d011f838a0b683c550f837e6544bc66dbfde950
BCChang <bcchang@utexas.edu>
2023-11-21 11:57:43 -0600

    Removing hydra dependence to test for now

--- server.py
+++ server.py
@@ -9,11 +9,13 @@ from omegaconf import DictConfig
 import flwr as fl
 import torch
 from agg_strats import SaveModelStrategy
-
+from flwr.server.strategy import FedAvg
+from network import MS_Net
 import utils
 
 
-@hydra.main(config_path="docs/config", config_name="config", version_base=None)
+
+@hydra.main(config_path="conf/", config_name="base", version_base=None)
 def main(cfg: DictConfig) -> None:
     """
     Load model for
@@ -25,19 +27,32 @@ def main(cfg: DictConfig) -> None:
     save_path = HydraConfig.get().runtime.output_dir
 
     # TODO: Either instantiate a new model or load from a checkpoint
-    model = hydra.utils.instantiate(cfg.model)
-
+    # model = hydra.utils.instantiate(cfg.model)
+    model = MS_Net(
+        num_scales=4,
+        num_features=1,
+        num_filters=2,
+        device='cpu',
+        f_mult=2,
+        summary=False
+    ).to("cpu")
+    #
     model_parameters = utils.get_model_parameters(model)
 
     # TODO: Instantiate a strategy
-    strategy = SaveModelStrategy(save_path=save_path)
-
+    model_dict = {'num_scales': 4,
+                  'num_features': 1,
+                  'num_filters': 2,
+                   'device': 'cpu',
+                   'f_mult': 2}
+    # strategy = SaveModelStrategy(save_path=save_path, model_dict=model_dict)
+    strategy = FedAvg()
 
     # Start Flower server for four rounds of federated learning
     fl.server.start_server(
         server_address="0.0.0.0:8080",
         config=fl.server.ServerConfig(num_rounds=cfg.num_rounds),
-        strategy=SaveModelStrategy,
+        strategy=strategy,
         # TODO: Add security certificates here if needed
     )
 
