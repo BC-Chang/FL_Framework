d01e97682a52c4c4214b4c475534ff3466331051
Bernard Chang <bcchang@utexas.edu>
2023-11-27 11:46:41 -0600

    Change default run configs and add client training files for testing

--- write_data_input_yaml.py
+++ write_data_input_yaml.py
@@ -2,9 +2,10 @@ from utils import write_yaml
 import pandas as pd
 
 if __name__ == "__main__":
-    train_df = pd.read_excel(r"C:\Users\bchan\Box\Research\Conference_Stuff\AGU_2023\results\train.xlsx", index_col=0)
-    val_df = pd.read_excel(r"C:\Users\bchan\Box\Research\Conference_Stuff\AGU_2023\results\val.xlsx", index_col=0)
+    client = "c2"
+    train_df = pd.read_excel(fr"C:\Users\bchan\Box\Research\Conference_Stuff\AGU_2023\results\train_{client}.xlsx", index_col=0)
+    val_df = pd.read_excel(fr"C:\Users\bchan\Box\Research\Conference_Stuff\AGU_2023\results\val_{client}.xlsx", index_col=0)
     test_df = pd.read_excel(r"C:\Users\bchan\Box\Research\Conference_Stuff\AGU_2023\results\test.xlsx", index_col=0)
-    print(train_df['MSNet_Name'])
-    write_yaml([train_df['MSNet_Name'], val_df['MSNet_Name']], ["train", "val"], "train_data.yml")
-    write_yaml([test_df['MSNet_Name']], ["test"], "test_data.yml")
+
+    write_yaml([train_df['MSNet_Name'], val_df['MSNet_Name']], ["train", "val"], f"{client}_train_data.yml")
+    # write_yaml([test_df['MSNet_Name']], ["test"], "test_data.yml")
