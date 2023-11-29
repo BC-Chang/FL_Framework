27ce10f8920174f4713cccf399d07b16bed757b3
BCChang <bcchang@utexas.edu>
2023-11-29 15:39:16 -0600

    Add array names and transforms to yaml dump

--- utils.py
+++ utils.py
@@ -42,11 +42,17 @@ def get_model_parameters(model: torch.nn.Module):
     return [val.cpu().numpy() for _, val in model.state_dict().items()]
 
 
-def write_yaml(data_list: list, phases: list, filename: str):
+def write_yaml(data_list: list, phases: list, filename: str,
+               x_array: str = 'bin', y_array: str = 'phi', x_xform: str = 'None', y_xform: str = 'None'):
     """
     Convert data from list of lists to yaml file.
     :param data_list: List of lists for each phase. Must match length of phases and the order of phases.
     :param phases: List of phases, i.e. ["train", "val"]
+    :param filename: Name of file to write to.
+    :param x_array: Name of x variable.
+    :param y_array: Name of y variable.
+    :param x_xform: Transformation to apply to x variable.
+    :param y_xform: Transformation to apply to y variable.
     """
 
     assert len(data_list) == len(phases), "Length of data_list must match length of phases."
@@ -65,6 +71,11 @@ def write_yaml(data_list: list, phases: list, filename: str):
         data_dict[f"{phase}_subsample"] = subsample
         data_dict[f"{phase}_size"] = size
         data_dict[f"{phase}_pressure"] = pressure
+
+    data_dict["x_array"] = [x_array]
+    data_dict["y_array"] = [y_array]
+    data_dict["x_xform"] = [x_xform]
+    data_dict["y_xform"] = [y_xform]
 
     with open(f"./data_input_files/{filename}", "w") as f:
         yaml.dump(data_dict, f)
