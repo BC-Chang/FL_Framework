ef0a79f248b9e67f5a49844a690d26942f54134e
BCChang <bcchang@utexas.edu>
2023-11-28 20:57:05 -0600

    Add velocity to data loading utils

--- dataloading_utils.py
+++ dataloading_utils.py
@@ -51,7 +51,8 @@ def load_samples(feat, sample_name, net_dict, xform=None):
     # TODO: Add data location to hydra config file
     data_dict = {'bin': {'path': f'{net_dict["data_loc"]}/bin', 'dkey': 'bin', 'ext': 'bin'},
                  'phi': {'path': f'{net_dict["data_loc"]}/elec', 'dkey': 'phi', 'ext': 'elec'},
-                 'Iz': {'path': f'{net_dict["data_loc"]}/elec', 'dkey': 'Iz', 'ext': 'elec'}}
+                 'Iz': {'path': f'{net_dict["data_loc"]}/elec', 'dkey': 'Iz', 'ext': 'elec'},
+                 'vel': {'path': f'{net_dict["data_loc"]}/vel', 'dkey': 'uz', 'ext': 'vel'}}
 
     path = data_dict.get(feat, data_dict.get('bin'))['path']
 
