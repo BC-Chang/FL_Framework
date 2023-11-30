4b96ac8ec89ab19286ddceff80963a9eb04904f7
Bernie <bcchang@utexas.edu>
2023-11-30 13:20:24 -0600

    Normalize losses by batch size

--- batchjob.sh
+++ batchjob.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH -J uni_data
-#SBATCH -o outfiles/uni_data_out.log
-#SBATCH -e errfiles/uni_data_err.log
-#SBATCH -p gpu-a100-dev
+#SBATCH -J fedProx
+#SBATCH -o outfiles/fedProx_out.log
+#SBATCH -e errfiles/fedProx_err.log
+#SBATCH -p gpu-a100
 #SBATCH -N 1
 #SBATCH -n 128
-#SBATCH -t 2:00:00
+#SBATCH -t 24:00:00
 #SBATCH -A OTH21076
 #SBATCH --mail-user=bcchang@utexas.edu
 #SBATCH --mail-type=all
@@ -16,8 +16,8 @@ source /work/06898/bchang/ls6/miniconda3/etc/profile.d/conda.sh
 conda activate fl
 
 
-CUDA_VISIBLE_DEVICES=0 python server.py device="cuda" test_input_file=test_data.yml data_loc=$SCRATCH/data num_rounds=500 &
-CUDA_VISIBLE_DEVICES=1 python client.py device="cuda" train_input_file=train_data.yml data_loc=$SCRATCH/data &
-CUDA_VISIBLE_DEVICES=2 python client.py device="cuda" train_input_file=train_data.yml data_loc=$SCRATCH/data &
+CUDA_VISIBLE_DEVICES=0 python server.py device="cuda" test_input_file=test_msnet.yml data_loc=$SCRATCH/data num_rounds=50 config_fit.local_epochs=10 strategy=fedprox &
+CUDA_VISIBLE_DEVICES=1 python client.py device="cuda" train_input_file=train_val_msnet.yml data_loc=$SCRATCH/data &
+CUDA_VISIBLE_DEVICES=2 python client.py device="cuda" train_input_file=train_val_msnet.yml data_loc=$SCRATCH/data &
 
 wait
