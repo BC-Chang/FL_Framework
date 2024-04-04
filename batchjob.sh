ba14990c93059780315a284214d7f0c9e9ea2764
Bernie <bcchang@utexas.edu>
2024-04-04 11:18:16 -0500

    Change run commands to use input files referring to tiff data

--- batchjob.sh
+++ batchjob.sh
@@ -12,12 +12,13 @@
 #SBATCH --mail-type=all
 
 #source /work/06898/bchang/ls6/ms_net_mostcurrent1/ms-net/bin/activate
-source /work/06898/bchang/ls6/miniconda3/etc/profile.d/conda.sh
-conda activate fl
+#source /work/06898/bchang/ls6/miniconda3/etc/profile.d/conda.sh
+#conda activate fl
 
 
-CUDA_VISIBLE_DEVICES=0 python server.py device="cuda" test_input_file=test_data.yml data_loc=$SCRATCH/data num_rounds=500 &
-CUDA_VISIBLE_DEVICES=1 python client.py device="cuda" train_input_file=train_data.yml data_loc=$SCRATCH/data &
-CUDA_VISIBLE_DEVICES=2 python client.py device="cuda" train_input_file=train_data.yml data_loc=$SCRATCH/data &
-
+CUDA_VISIBLE_DEVICES=0 python server.py device="cuda" test_input_file=server_tiff_net.yml data_loc=$SCRATCH/data num_rounds=100 &
+sleep 5
+CUDA_VISIBLE_DEVICES=1 python client.py device="cuda" train_input_file=client_tiff_net.yml data_loc=$SCRATCH/data &
+sleep 5
+CUDA_VISIBLE_DEVICES=2 python client.py device="cuda" train_input_file=client_tiff_net.yml data_loc=$SCRATCH/data &
 wait
