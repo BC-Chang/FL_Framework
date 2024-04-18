fd68b2b259ed593ca75b2481a3794531ba03d6fe
bcchang <bcchang@utexas.edu>
2024-04-18 16:15:48 -0500

    Update main script to load weights from the server

--- main_lightning.py
+++ main_lightning.py
@@ -1,3 +1,4 @@
+import torch
 from network import MS_Net
 from lightning import Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint
@@ -15,33 +16,36 @@ import load_data
 @hydra.main(config_path="conf/", config_name="base", version_base=None)
 def main(cfg: DictConfig) -> None:
     # Look for an updated checkpoint, instantiate a new model if it does not exist
-    try:
-        model_dir = Path(f'lightning_logs/round_{cfg.round}')
-        model_loc = list(model_dir.glob('*.ckpt'))[-1]
-        print(f"Attempting model load from {str(model_dir)}")
 
-        yaml_dict = load_data.load_hparams(list(model_dir.glob("lightning_logs/version_*/hparams.yaml"))[-1])
-        model = MS_Net.load_from_checkpoint(model_loc,
-                                          net_name=yaml_dict['net_name'],
-                                          num_scales=yaml_dict['num_scales'],
-                                          num_features=yaml_dict['num_features'],
-                                          num_filters=yaml_dict['num_filters'],
-                                          f_mult=yaml_dict['f_mult'],
-                                          summary=False)
+    model = instantiate(cfg.model).to(cfg.device)
+
+    try:
+        model_dir = Path(f'server_models/')
+        model_loc = list(model_dir.glob(f'server_round_{cfg.round}.ckpt'))[-1]
+        print(f"Attempting model load from {str(model_dir)}")
+        ckpt = torch.load(model_loc)
+        model.load_state_dict(ckpt)
+        # yaml_dict = load_data.load_hparams(list(model_dir.glob("lightning_logs/version_*/hparams.yaml"))[-1])
+        # model = MS_Net.load_from_checkpoint(model_loc,
+        #                                     num_scales=cfg.model['num_scales'],
+        #                                     num_features=cfg.model['num_features'],
+        #                                     num_filters=cfg.model['num_filters'],
+        #                                     f_mult=cfg.model['f_mult'],
+        #                                     summary=False)
 
         print(f"Model loaded successfully!")
 
     except (FileNotFoundError, IndexError):
         print("No checkpoint found, instantiating a new model...")
         print(cfg.model)
-        model = instantiate(cfg.model).to(cfg.device)
+
         model_loc = None
 
     # Load the datasets
     trainset, valset = load_data.load_data(cfg.train_input_file, path_to_data=cfg.data_loc, phases=["train", "val"])
 
     # Callbacks
-    callbacks = [ModelCheckpoint(dirpath=f"lightning_logs/round_{cfg.round+1}",
+    callbacks = [ModelCheckpoint(dirpath=f"client_model/round_{cfg.round+1}",
                                  save_on_train_epoch_end=True,
                                  save_weights_only=True)]
 
@@ -51,7 +55,7 @@ def main(cfg: DictConfig) -> None:
                       min_epochs=1,
                       accumulate_grad_batches=cfg.config_fit.batch_size,
                       check_val_every_n_epoch=cfg.config_fit.val_interval,
-                      default_root_dir=f"lightning_logs/round_{cfg.round+1}",
+                      default_root_dir=f"client_model/round_{cfg.round+1}",
                       callbacks=callbacks,
                       accelerator=cfg.device)
 
@@ -59,7 +63,7 @@ def main(cfg: DictConfig) -> None:
     trainer.fit(model, trainset, valset) #, ckpt_path=model_loc)
 
     # Write the training set size to a text file
-    with open(f"lightning_logs/round_{cfg.round+1}/training_size.txt", "w") as f:
+    with open(f"client_model/round_{cfg.round+1}/training_size.txt", "w") as f:
         f.write(f"{len(trainset)}")
 
 
